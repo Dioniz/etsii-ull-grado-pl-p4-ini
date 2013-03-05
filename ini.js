1d0d54640d199c4454411d92b59b7f9f7218f2c7
Dioniz <frandioniz@gmail.com>
2013-03-05 11:43:41 +0000

    Update ini.js

--- ini.js
+++ ini.js
@@ -1,4 +1,3 @@
-[~/Dropbox/src/javascript/PLgrado/ini(master)]$ cat ini.js 
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 
 $(document).ready(function() {
@@ -17,8 +16,8 @@ function calculate(evt) {
       var pretty = tokensToString(tokens);
       
       out.className = 'unhidden';
-      initialinput._________ = contents;
-      finaloutput._________ = pretty;
+      initialinput.value = contents;
+      finaloutput.value = pretty;
     }
     r.readAsText(f); // Leer como texto
   } else { 
@@ -26,48 +25,50 @@ function calculate(evt) {
   }
 }
 
-var temp = '<li> <span class = "<%= ______ %>"> <%= _ %> </span>\n';
+var temp = '<li> <span class = "<%= token.type %>"> <%= matching %> </span>\n';
 
 function tokensToString(tokens) {
    var r = '';
    for(var i in tokens) {
      var t = tokens[i];
      var s = JSON.stringify(t, undefined, 2);
-     s = _.template(temp, {t: t, s: s});
+     s = _.template(temp, {token: t, matching: s});   //_  se usa para hacer referencia a un metodo de la libreria underscore(template interpola lo que haya entre % en la cadena temp)
+							//ttoken:--> Lo que hay que decir en el template,  t--> la cadena por la que se reemplaza
      r += s;
    }
    return '<ol>\n'+r+'</ol>';
 }
 
 function lexer(input) {
-  var blanks         = /^___/;
-  var iniheader      = /^________________/;
-  var comments       = /^________/;
-  var nameEqualValue = /^________________________/;
-  var any            = /^_______/;
+  var blanks         = /^\s+/g;
+  var iniheader      = /^\[.+\]/;
+  var comments       = /^[;].*/;
+  var nameEqualValue = /^([^=]+)\s*(\=)\s*(.*)/g;	//   /^(([^=;\r\n]+)=([^;\r\n]*)/;
+  var any            = /^(\S.*)/;
 
   var out = [];
   var m = null;
 
   while (input != '') {
-    if (m = blanks.____(input)) {
-      input = input.substr(m.index+___________);
-      out.push({ type : ________, match: _ });
+    if (m = blanks.exec(input)) {
+      input = input.substr(m.index+m.lastIndex);
+      out.push({ type : input, match: m });	// type : ________
     }
     else if (m = iniheader.exec(input)) {
-      input = input.substr(___________________);
-      _______________________________________ // avanzemos en input
+      input = input.substr(m.index+m.lastIndex);
+      input.lastIndex;// avanzemos en input
     }
     else if (m = comments.exec(input)) {
-      input = input.substr(___________________);
-      _________________________________________
+      input = input.substr(m.index+m.lastIndex);
+      alert(input);
+      input.lastIndex;// avanzemos en input
     }
     else if (m = nameEqualValue.exec(input)) {
-      input = input.substr(___________________);
-      _______________________________________________
+      input = input.substr(m.index+m.lastIndex);
+      input.lastIndex;// avanzemos en input
     }
     else if (m = any.exec(input)) {
-      _______________________________________
+      input.lastIndex;// avanzemos en input
       input = '';
     }
     else {
